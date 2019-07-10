8d87420dc74d9b4157bff4fa15e4661e9a9bf6de
Gustavo Gonçalves <45770155+Guztavosw@users.noreply.github.com>
2019-07-10 17:06:31 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,27 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+ 
+var array = [1, 2, 3, 4, 5]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+// 
+function myFunction2(arg){
+... return arg;}
+
+myFunction(array)
+[ 1, 2, 3, 4, 5 ]
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+function myFunction2(arg){
+  return arg[1];
+}
+
+myFunction2(array); // 2
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -21,18 +31,34 @@ no primeiro parâmetro. O índice usado para retornar o valor, deve ser o númer
 segundo parâmetro.
 */
 // ?
-
+function otherFunction(arg, index){
+... return arg [ index ];
+}
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
 // ?
-
+var valores = [ 'gustavo', 2.5, true, [1, 2, 'Gustavo'], {b :2} ];
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
 // ?
+> console.log( otherFunction(valores, 1)  ); // gustavo
+gustavo
+> console.log( otherFunction(valores, 2)  ); // 2.5
+2.5
+undefined
+> console.log( otherFunction(valores, 4)  );
+undefined
 
+undefined
+> console.log( otherFunction(valores, 3)  );
+true
+undefined
+
+> console.log( otherFunction(valores, 5)  );
+ NaN
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
 livro. Dentro dessa função, declare uma variável que recebe um objeto com as
